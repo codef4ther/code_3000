ad6b68a2fe63675f1a7af10b5255f775202a6ce0
codef4ther <138262232+codef4ther@users.noreply.github.com>
2026-02-12 02:13:43 +0000

    b

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,10 +11,10 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.02,
-        n_estimators=4000,
-        max_depth=2,
-        subsample=0.7,
-        min_samples_leaf=10,
+        n_estimators=6000,
+        max_depth=1,
+        subsample=0.8,
+        min_samples_leaf=20,
         random_state=seed
     )
     model.fit(X, y)
